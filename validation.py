f728539f5186c6b006bc48a86dc843d8d640d169
Clemens Bonse <clemensbonse@web.de>
2018-03-22 12:04:28 +0100

    Added batch-simulation Mode, indicated by -b commandline-argument. Changed conda-environment name, validation.py and moved Simulation-Management inside trajectoryGenerator.py.

--- validation.py
+++ validation.py
@@ -1,7 +1,8 @@
 r"""Validation of traces/points against an Occupancy-grid."""
 import wx
+import warnings
 
-def validate_trace(trace_x, trace_y, worldMap):
+def validate_trace(trace_x, trace_y, worldMap, batch):
     r"""Check if a trace is valid against an Occupancy-grid.
 
     Parameters
@@ -10,6 +11,10 @@ def validate_trace(trace_x, trace_y, worldMap):
         Lists containing the individual values of the trace.
     worldMap : *array*
         Occupancy-grid from `Sillywalks.sillywalks.mapCreation.image2array`.
+    batch : *boolean*
+        Indicates if the code is running in batch-mode. Necessary for
+        error-indication since in this case a `wx.MessageBox` is not
+        wanted.
 
     Returns
     -------
@@ -20,19 +25,24 @@ def validate_trace(trace_x, trace_y, worldMap):
     assert (len(trace_x) > 0) and (len(trace_y) > 0) and (len(trace_x) == len(trace_y))
     valid = True
     for x, y in zip(trace_x, trace_y):
-        if validate_point(x, y, worldMap) == False:
+        if validate_point(x, y, worldMap, batch) == False:
             valid = False
     return valid
 
-def validate_point(x, y, worldMap):
+def validate_point(x, y, worldMap, batch):
     r"""Check if a pixel in the Occupancy-grid is occupied.
 
+    If Coordinates fall outside of the grid-map, True is returned!
     Parameters
     ----------
     x, y : *int*/*int*
         Pixel-coordinates
     worldMap : *array_like*
         Occupancy-grid from `Sillywalks.sillywalks.mapCreation.image2array`.
+    batch : *boolean*
+        Indicates if the code is running in batch-mode. Necessary for
+        error-indication since in this case a `wx.MessageBox` is not
+        wanted.
 
     Returns
     -------
@@ -45,20 +55,31 @@ def validate_point(x, y, worldMap):
         y = int(y)
     except:
         msg = "Could not convert Trace-positions to integer: "+str(x)+" - "+str(y)
+        if batch:
+            print(msg)
+        else:
+            wx.MessageBox(msg, 'Error', \
+                          wx.OK | wx.ICON_ERROR)
         raise ValueError(msg)
     ysize, xsize = worldMap.shape
     if not (0 <= x < xsize):
         msg = "Invalid Trace-X-Position: "+str(x)+" with Image-X-Size: "+str(xsize)
         msg += "\nMaybe modify the trace-points or reduce noise to create a valid trace."
-        wx.MessageBox(msg, 'Error', \
+        if batch:
+            warnings.warn(msg)
+        else:
+            wx.MessageBox(msg, 'Error', \
                           wx.OK | wx.ICON_ERROR)
-        raise ValueError(msg)
+            return True
     if not (0 <= y < xsize):
         msg = "Invalid Trace-Y-Position: "+str(y)+" with Image-Y-Size: "+str(ysize)
         msg += "\nMaybe modify the trace-points or reduce noise to create a valid trace."
-        wx.MessageBox(msg, 'Error', \
+        if batch:
+            warnings.warn(msg)
+        else:
+            wx.MessageBox(msg, 'Error', \
                           wx.OK | wx.ICON_ERROR)
-        raise ValueError(msg)
+        return True
     if worldMap[y][x] == 1:
         return False
     else:
